fd2ec78d0b836b3073340a5c0db93b40c25e6faa
AdamZhou1029 <yizhouzhou@yizhous-MacBook-Pro.local>
2022-12-27 15:50:46 -0600

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
